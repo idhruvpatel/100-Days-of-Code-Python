83af7cd6d2f821c49876b8c7d750e30f41689fee
Dhruv Patel <dhruv.patel@appdirect.com>
2021-06-09 13:05:29 +0530

    Day 1 Content of the course.

---
+++ Day 1 - Working with Variables in Python to Manage Data/Debugging_Exercise.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation\n" +
+      "String Concatination is done with '+' sign.\n" +
+      'e.g. print("Hellow" + "world")\n' +
+      "New lines can be created with a backslash and n.")
