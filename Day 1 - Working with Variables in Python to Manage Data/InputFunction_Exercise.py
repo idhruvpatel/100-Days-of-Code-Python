813245d32589ff7873e9b8f57442848e3feb889b
Dhruv Patel <dhruv.patel@appdirect.com>
2021-06-09 16:47:31 +0530

    Added the band name generator and several other exercises

---
+++ Day 1 - Working with Variables in Python to Manage Data/InputFunction_Exercise.py
@@ -0,0 +1,24 @@
+# Inputs
+# Instructions
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+# e.g.
+
+# https: // www.google.com/search?q = how+to+get+the+length+of+a+string+in+python+stack+overflow
+
+# Warning. Your program should work for different inputs. e.g. any name that you input.
+
+# Example Input
+# Angela
+
+# Example Output
+# 6
+# e.g. When you hit run, this is what should happen:
+
+# https: // cdn.fs.teachablecdn.com/opevxYZSTM2ZHjbAX3XV
+
+# Hint
+# You can put functions inside other functions.
+# Don't try to print anything other than the length.
+
+print(len(input("What is your name?: ")))
